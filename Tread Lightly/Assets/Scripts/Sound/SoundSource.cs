022aa26a8283cb13402bc6ff545d921a06da79ef
Jack Radforth <57113334+RadforthJack@users.noreply.github.com>
2022-11-13 19:33:07 +0000

    Added more settings to the sound sources

--- Tread Lightly/Assets/Scripts/Sound/SoundSource.cs
+++ Tread Lightly/Assets/Scripts/Sound/SoundSource.cs
@@ -17,4 +17,26 @@ public static class SoundSourceExtensions
             _ => 0f,
         };
     }
+
+    public static float GetDefaultVolume(this SoundSource soundSource)
+    {
+        return soundSource switch
+        {
+            SoundSource.Walking => 1f,
+            SoundSource.Falling => 4f,
+            SoundSource.Running => 2f,
+            _ => 0f,
+        };
+    }
+
+    public static float GetFrequency(this SoundSource soundSource)
+    {
+        return soundSource switch
+        {
+            SoundSource.Walking => 0.5f,
+            SoundSource.Falling => 0.25f,
+            SoundSource.Running => 0.25f,
+            _ => 0f,
+        };
+    }
 }
